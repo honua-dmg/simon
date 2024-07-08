2a28d50447d317af8ac3c60ce2abe19496eef6e3
Guru Sai Prasad Mallepalli Gajula <glavnihonua@gmail.com>
2024-07-08 05:44:36 -0400

    get random int func copied from MDN docs

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
+function getRandomInt(min, max) {
+    const minCeiled = Math.ceil(min);
+    const maxFloored = Math.floor(max);
+    return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled); // The maximum is exclusive and the minimum is inclusive
+  }
+  
+
+
+
+var order = [];
+
 $('.btn').click(
     (e)=>{
         console.log(e.target.getAttribute('id'));
+        order.push(e.target.getAttribute('id'));
+        console.log(order)
     }
 );
+
+/*
+overview:
+- figure out how to call random numbers
+- figure out how to remember how to store inputs (order of keys pressed)
+- 
+*/
+
